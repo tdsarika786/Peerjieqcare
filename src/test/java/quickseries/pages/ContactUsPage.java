17bdb86ce261cdea24f74a03f85629096767bef0
Sarika <dhallsarika786@gmail.com>
2020-06-10 00:34:00 -0400

    Add files

--- src/test/java/quickseries/pages/ContactUsPage.java
+++ src/test/java/quickseries/pages/ContactUsPage.java
@@ -1,16 +1,12 @@
 package quickseries.pages;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 
 import quickseries.factories.DataProviderFactory;
--- src/test/java/quickseries/testcases/ContactUsScenarios.java
+++ src/test/java/quickseries/testcases/ContactUsScenarios.java
@@ -5,100 +5,56 @@ import org.testng.annotations.Test;
 
 import quickseries.pages.ContactUsPage;
 import quickseries.pages.HomePage;
-import quickseries.factories.DataProviderFactory;
 import quickseries.pages.BaseClass;
-
 
 public class ContactUsScenarios extends BaseClass {
 
 	ContactUsPage contact;
-	
+
 	HomePage home;
 
 	@Test(priority = 0)
 	public void verifyPage() {
 
 		contact = PageFactory.initElements(driver, ContactUsPage.class);
-		
-		home = PageFactory.initElements(driver, HomePage.class);
-		
-        home.verifyUrl();	
 
-	    logger = report.createTest("Quickseries Contact Us URL validation");
-	    
+		home = PageFactory.initElements(driver, HomePage.class);
+
+		home.verifyUrl();
+
+		logger = report.createTest("Quickseries Contact Us URL validation");
+
 		logger.info("Validating Quickseries url");
-				
+
 		contact.navigateToConatctUsPage();
 
 		contact.verifyContactUsUrl();
 
 		logger.info("Validated Contact us page url");
-		
+
 		contact.verifyPageTitleAfterNavigateToContactUs();
-		
+
 		logger.info("Validated contact us page title");
-		
+
 		contact.verifyHeadingAfterNavigateToDept();
-		
+
 		logger.info("Validated contact us page headding");
-		
-		
+
 	}
-	
-	
-	@Test( priority = 1, dependsOnMethods = "verifyPage")
+
+	@Test(priority = 1, dependsOnMethods = "verifyPage")
 	public void TC_01_Display_Validation_Errors() {
 
 		logger = report.createTest("Errors Validation");
-		
+
 		contact.validateContactPageErrors();
-		
+
 		logger.info("Validated UI Error messages displayed");
-		
+
 		contact.verifyContactUsUrl();
-		
+
 		logger.info("User unable to submit form");
-		
-		
-	}
-
-	// Dept Page - Create Department
-
-	/*
-	@Test( priority = 1, dependsOnMethods = "verifyPage")
-	public void TC_01_Department_Create() {
-
-		logger = report.createTest("Create Department Scenario");
-		
-		logger.info("Login as Account Administrator");
-
-		login.loginToApplication(DataProviderFactory.getExcel().getCellData("Dev", 1, 0),
-
-				DataProviderFactory.getExcel().getCellData("Dev", 1, 1));
-
-		login.verifyUrlAfterLogin();
-
-		logger.info("Logged in");
-
-		role.selectRoleAsAccountAdmin();
-
-		logger.info("Account Administrator role selected");
-
-		dept.navigateToDepartmentsPage();
-
-		dept.verifyUrlAfterNavigateToDept();
-
-		logger.info("Department Page");
-
-		dept.AddDepartmentsPage();
-
-		logger.info("Department Added");
-
-		dept.verifyDeparment("Safety_peerjiAdd");
-
-		logger.info("Department adddition verified");
 
 	}
-*/
 
 }
