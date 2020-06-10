60f60c37fe77c6a2d2f7759962ba661784985445
Sarika <dhallsarika786@gmail.com>
2020-06-10 00:33:20 -0400

    Task completed - Automation scripts for contact us page.

---
+++ src/test/java/quickseries/factories/DataProviderFactory.java
@@ -0,0 +1,17 @@
+package quickseries.factories;
+
+public class DataProviderFactory {
+
+	
+	
+	
+	public static ConfigProvider getConfig()
+	{
+		ConfigProvider config=new ConfigProvider();
+		
+		return config;
+	}
+	
+	
+	
+}
